fa8eca5931943ae47995dab0bfff1d9c628adf28
Leonardo Souza <leuhsouza@gmail.com>
2023-11-03 06:22:58 -0300

    linear search

--- linear search.py
+++ linear search.py
@@ -1,9 +1,23 @@
 from random import randint
 
-lista = ()
+lista = []
 
 for n in range(0,10):
-    lista = randint(0,10)
+    lista.append (randint(0,10))
+
 
 print (lista)
 
+procurado = int(input('Digite um valor a ser pesquisado: '))
+
+def linear_search(lista,procurado):
+for indice, valor in enumerate(lista):
+    if valor == procurado:
+        return f'O valor procurado está na posição: {indice}'
+    return 'Valor não escontrado na lista'
+
+
+    
+    '''if procurado == valor:
+        print(f'O valor procurado está na posição {indice}')
+    else'''
