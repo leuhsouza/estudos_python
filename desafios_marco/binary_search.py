3a44f73651c50ec48f385c5f0266e2ff81ce260f
Leonardo Souza <leuhsouza@gmail.com>
2023-11-17 22:37:00 -0300

    binary_search

---
+++ desafios_marco/binary_search.py
@@ -0,0 +1,14 @@
+from random import randint
+
+search = int(input('Digite um numero para pesquisa-lo'))
+
+minha_lista = []
+
+for n in range (0,10):
+    minha_lista.append (randint(0,100))
+
+print(len(minha_lista))
+
+
+mid = 
+print(minha_lista)
