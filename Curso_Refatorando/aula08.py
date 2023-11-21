31807a0ac6be9178a4cbb84b19fae0c9df869133
Leonardo Souza <leuhsouza@gmail.com>
2023-11-20 23:36:56 -0300

    aula refatorando

---
+++ Curso_Refatorando/aula08.py
@@ -0,0 +1,52 @@
+# tenho_sede = True
+
+# if tenho_sede:
+#     print("beber Agua")
+
+# print("Vida que segue")
+
+# esta_frio = False
+
+# if esta_frio:
+#     print("vestir um casaco")
+# else:
+#     print("Vestir camiseta")
+
+tenho_sede = True
+tenho_fome = False
+
+estou_dieta = True
+
+# if tenho_sede or tenho_fome:
+#     print("vou na cozinha")
+# else:
+#     print("ficar vendo netflix")
+
+# if tenho_sede and tenho_fome:
+#     print("fazer sanduiche e coca")
+# else:
+#     print("Nao tenho fome ou sede")
+
+# if tenho_sede and tenho_fome:
+#     print("fazer sanduiche e coca")
+# elif tenho_sede and not (tenho_fome):
+#     if estou_dieta:
+#         print("tomar agua")
+#     else:
+#         print("tomar uma coquinha")
+
+# elif not(tenho_sede) and tenho_fome:
+#     print("fazer um sanduiche")
+# else:
+#     print("Ficar vendo netflix")
+
+num1 = 5
+num2 = 32
+
+if num1 == num2:
+    print("num1 é igual num2")
+# elif num1 != num2:
+#     print("num1 é difernte de num2")
+elif num >  num2:
+    print("num1 é maior que num 2")
+elif num1 
