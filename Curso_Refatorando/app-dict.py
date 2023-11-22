98b585f5b621cda9b571ea2221bc17375c6174be
Leonardo Souza <leuhsouza@gmail.com>
2023-11-21 22:13:58 -0300

    aula_refatorando

---
+++ Curso_Refatorando/app-dict.py
@@ -0,0 +1,17 @@
+meses = {
+    "Jan" : "Janeiro",
+    "Fev" : "Feveiro",
+    "Mar" : "Março",
+    "Abr" : "Abril",
+    "Mai" : "Maio",
+    "Jun" : "Junho",
+    "Jul" : "Julho",
+    "Ago" : "Agosto",
+    "Set" : "Setembro",
+    "Out" : "Outubro",
+    "Nov" : "Novembro",
+    "Dez" : "Dezembro",
+}
+
+print(meses["Jan"])
+print(meses.get("Jan"))
---
+++ Curso_Refatorando/aula10.py
@@ -0,0 +1,13 @@
+# frutas = {"maça","Laranja","Abacaxi"}
+
+# print(frutas)
+
+# frutas.add("Pera")
+
+# frutas.remove("maça")
+# frutas.pop()
+# print(frutas)
+
+set = {"maça", "laranja", "Abacaxi"}
+set2 = {0,3,50,-74}
+set3 =  {True,False}
