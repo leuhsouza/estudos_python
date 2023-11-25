7f0efae17ee003097bc6f04066be8811c562ec22
Leonardo Souza <leuhsouza@gmail.com>
2023-11-24 23:00:35 -0300

    aula refatorando

---
+++ Curso_Refatorando/aula14/app.py
@@ -0,0 +1,28 @@
+from carro import Carro
+
+fusca = Carro("Volks", "Fusca", "Azul", "Gasolina")
+
+# fusca.ligar()
+# fusca.ligar()
+# fusca.acelerar()
+# fusca.acelerar()
+# fusca.acelerar()
+# fusca.acelerar()
+# fusca.acelerar()
+# fusca.acelerar()
+# fusca.acelerar()
+# fusca.acelerar()
+# fusca.frear()
+# fusca.frear()
+# fusca.frear()
+# fusca.frear()
+# fusca.frear()
+# fusca.frear()
+# fusca.desligar()
+
+
+ferrari = Carro("ferrari", "Ferrari", "Vermelho", "Gasolina")
+tesla = Carro("Tesla", "Cybertruck", "cinza", "Eletrico")
+
+ferrari.ligar()
+tesla.ligar()
