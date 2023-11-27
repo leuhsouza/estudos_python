6af263f7dc214ffefdf2462bc34900621d9a2efe
Leonardo Souza <leuhsouza@gmail.com>
2023-11-26 22:31:14 -0300

    testes

--- desafios_marco/algoritmos/test_main.py
+++ desafios_marco/algoritmos/test_main.py
@@ -7,3 +7,8 @@ def test_func_deve_retornar_posic_valor_pesq():
     resultado_esperado = 3
     assert linear_search.linear_search(entrada,valor_pesq) == resultado_esperado
     
+def test_func_deve_retornar_nao_encontrado_se_valor_nao_presente():
+    entrada = [2, 3, 4, 10, 40]
+    valor_pesq = 20
+    resultado_esperado = -1
+    assert linear_search.linear_search(entrada, valor_pesq) == resultado_esperado
